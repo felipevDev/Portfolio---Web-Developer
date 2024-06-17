7b2e330f91cc730bbea62b58abec3ea2f292b6b8
Felipe Vieira <126516326+felipevDev@users.noreply.github.com>
2024-06-17 14:59:39 -0300

    edição

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/felipevDev/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/felipevDev/Portfolio---Web-Developer/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
